f08c88fb50e2d7f889faedc8d273509a5fdd2d5e
Gouri Panda <gouripanda4@gmail.com>
2022-11-22 23:43:51 +0530

    Migrated LanguageEntity from ObjectBox to Room

--- core/src/main/java/org/kiwix/kiwixmobile/core/dao/entities/LanguageEntity.kt
+++ core/src/main/java/org/kiwix/kiwixmobile/core/dao/entities/LanguageEntity.kt
@@ -18,17 +18,15 @@
 
 package org.kiwix.kiwixmobile.core.dao.entities
 
-import io.objectbox.annotation.Convert
-import io.objectbox.annotation.Entity
-import io.objectbox.annotation.Id
-import io.objectbox.converter.PropertyConverter
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
 import org.kiwix.kiwixmobile.core.zim_manager.Language
 import java.util.Locale
 
-@Entity
+@androidx.room.Entity
 data class LanguageEntity(
-  @Id var id: Long = 0,
-  @Convert(converter = StringToLocaleConverter::class, dbType = String::class)
+  @PrimaryKey var id: Long = 0,
+  // todo add converter in database class
   val locale: Locale = Locale.ENGLISH,
   val active: Boolean = false,
   val occurencesOfLanguage: Int = 0
@@ -45,10 +43,12 @@ data class LanguageEntity(
     Language(locale, active, occurencesOfLanguage, id)
 }
 
-class StringToLocaleConverter : PropertyConverter<Locale, String> {
-  override fun convertToDatabaseValue(entityProperty: Locale?): String =
+class StringToLocaleConverter {
+  @TypeConverter
+  fun convertLocaleToString(entityProperty: Locale?): String =
     entityProperty?.isO3Language ?: Locale.ENGLISH.isO3Language
 
-  override fun convertToEntityProperty(databaseValue: String?): Locale =
+  @TypeConverter
+  fun convertStringToLocale(databaseValue: String?): Locale =
     databaseValue?.let(::Locale) ?: Locale.ENGLISH
 }
